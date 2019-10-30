3b5c912ebe72d540bdf1dd5f1d0a4b80e2160a7f
Aric <1246893231@qq.com>
2019-10-30 22:33:43 +0800

    init

---
+++ WeiBo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeiBo
+//
+//  Created by 李喜明 on 2019/10/30.
+//  Copyright © 2019 李喜明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
